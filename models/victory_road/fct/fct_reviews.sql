76c2935253fdc05085deddf95bf70d377809a612
Ecequiel Chavez <ecequiel.chavez@dbtlabs.com>
2024-08-15 02:57:04 +0000

    macros and macro tests

--- models/victory_road/fct/fct_reviews.sql
+++ models/victory_road/fct/fct_reviews.sql
@@ -7,7 +7,10 @@
 WITH src_reviews AS (
  SELECT * FROM {{ ref('src_reviews') }}
 )
-SELECT * FROM src_reviews
+SELECT *
+-- now depreecated; need to remove NULLS and blanks
+--   {{ dbt_utils.generate_surrogate_key(['listing_id', 'review_date', 'review_date']) }} as review_id, 
+FROM src_reviews
 WHERE review_text is not null
 {% if is_incremental() %}
  AND review_date > (select max(review_date) from {{ this }})
