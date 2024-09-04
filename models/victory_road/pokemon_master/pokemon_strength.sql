62c3efe0fbb013244a8322e91891a20ffa237e47
Ecequiel Chavez <ecequiel.chavez@dbtlabs.com>
2024-09-04 16:11:54 +0000

    unit_test was done right!

--- models/victory_road/pokemon_master/pokemon_strength.sql
+++ models/victory_road/pokemon_master/pokemon_strength.sql
@@ -4,8 +4,8 @@ With a AS (SELECT
     TOTAL,
     ATTACK,
     CASE
-        WHEN ATTACK > 50 then 'Strong'
         WHEN ATTACK > 60 then 'Really Strong'
+        WHEN ATTACK > 50 then 'Strong'
         ELSE 'Weak'
         END as Power
 FROM {{ ref("pokemon_battle") }})
