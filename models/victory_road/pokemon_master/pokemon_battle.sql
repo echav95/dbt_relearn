a66a934bae91f6a0d65c14a076326921b0f23d62
Ecequiel Chavez <ecequiel.chavez@dbtlabs.com>
2024-09-06 04:51:13 +0000

    finished unit_testing

--- models/victory_road/pokemon_master/pokemon_battle.sql
+++ models/victory_road/pokemon_master/pokemon_battle.sql
@@ -4,5 +4,24 @@ WITH poke_data AS (
     WHERE name NOT LIKE '%Mega%'
 )
 
-SELECT *
+SELECT
+    id,
+    name,
+    type1,
+    type2,
+    total,
+    hp,
+    attack,
+    defense,
+    spatk,
+    spdef,
+    speed,
+    legendary,
+    generation,
+    CASE
+        WHEN generation = 1 THEN CAST('1996-02-27' AS DATE)
+        WHEN generation = 2 THEN CAST('1999-11-21' AS DATE)
+        WHEN generation = 3 THEN CAST('2003-07-18' AS DATE)
+        WHEN generation = 4 THEN CAST('2006-09-28' AS DATE)
+        END as Release_Date 
 FROM poke_data
--- models/victory_road/pokemon_master/pokemon_strength.sql
+++ models/victory_road/pokemon_master/pokemon_strength.sql
@@ -7,7 +7,9 @@ With a AS (SELECT
         WHEN ATTACK > 60 then 'Really Strong'
         WHEN ATTACK > 50 then 'Strong'
         ELSE 'Weak'
-        END as Power
+        END as Power,
+    Release_Date,
+    datediff('day', Release_Date, {{dbt.current_timestamp()}}) as days_since_released
 FROM {{ ref("pokemon_battle") }})
 
 SELECT *
