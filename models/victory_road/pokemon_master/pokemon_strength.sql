2dfb7b3aae9a5d72ce3388ee67741dec8892f57a
Ecequiel Chavez <ecequiel.chavez@dbtlabs.com>
2024-09-18 17:13:04 +0000

    fixed testing issues

--- models/victory_road/pokemon_master/pokemon_strength.sql
+++ models/victory_road/pokemon_master/pokemon_strength.sql
@@ -11,7 +11,7 @@ With battle As (Select
     End As power,
     datediff('day', release_date, {{ dbt.current_timestamp() }})
         As days_since_released
-From {{ ref("pokemon_battle") }}
+From {{ ref("pokemon_battle", v=1) }}
 )
 
 Select *
