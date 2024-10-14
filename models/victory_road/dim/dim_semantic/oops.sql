8ac40954f506d568d95912c5af7f3afc834b82d0
Ecequiel Chavez <ecequiel.chavez@dbtlabs.com>
2024-10-14 22:37:04 +0000

    ffff

--- models/victory_road/dim/dim_semantic/oops.sql
+++
@@ -1,6 +0,0 @@
-SELECT
-  DATE_TRUNC('day', created_at) AS metric_time__day
-  , AVG(price) AS price_avg
-FROM ANALYTICS.dbt_echavez.dim_listings_w_host listings_with_host_src_10000
-GROUP BY
-  DATE_TRUNC('day', created_at)
