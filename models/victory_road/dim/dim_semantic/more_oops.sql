86e6b3f43a872ddcabfe249b212cbed4b1a09f02
Ecequiel Chavez <ecequiel.chavez@dbtlabs.com>
2024-10-14 22:30:33 +0000

    gggg

--- models/victory_road/dim/dim_semantic/more_oops.sql
+++ models/victory_road/dim/dim_semantic/more_oops.sql
@@ -1,6 +1,6 @@
 SELECT
   DATE_TRUNC('day', created_at) AS metric_time__day
-  , AVG(price) AS price_avg
+  , MIN(minimum_nights) AS minimum_nights
 FROM ANALYTICS.dbt_echavez.dim_listings_w_host listings_with_host_src_10000
 GROUP BY
   DATE_TRUNC('day', created_at)
